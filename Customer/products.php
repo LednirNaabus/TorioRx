45f789aa2a4db115f2f9a725132b4f34f3510a19
Ayesha Keith Santos <ayeshakeithsantos@gmail.com>
2022-04-23 14:12:04 +0800

    customer include header

--- Customer/products.php
+++ Customer/products.php
@@ -2,16 +2,7 @@
     <head>
         <title>TorioRx Pharmacy | Products</title>
 
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
-		
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
+        <?php include("../Customer/includes/main/header.php"); ?>
 
         <?php include("../Customer/includes/main/products_style.php"); ?>
 
