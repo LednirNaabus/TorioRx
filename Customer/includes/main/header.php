0c5aeb0de94dfdadccb29c34fe607b401a9c6153
Gabriela Andes <gabriela.andes05@gmail.com>
2022-04-23 17:57:33 +0800

    Navbar

---
+++ Customer/includes/main/header.php
@@ -0,0 +1,4 @@
+<meta charset = "utf-8" />
+<meta name = "viewport" content = "width=device-width, initial-scale=1, maximum-scale=5" />
+<link rel="stylesheet" href="dist/css/style.css">
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
---
+++ Customer/user-dashboard.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>User Dashboard</title>
+        <?php include('includes/main/header.php'); ?>
+    </head>
+    <body>
+        <!-- navbar -->
+        <?php include('includes/main/navbar.php'); ?>
+    </body>
+</html>
