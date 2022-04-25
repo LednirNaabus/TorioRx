0bf0f1972205448e21ef9eb5a8980437391871c5
Ayesha Keith Santos <ayeshakeithsantos@gmail.com>
2022-04-25 21:43:31 +0800

    navbar quick fix for view product

--- Customer/viewproduct.php
+++ Customer/viewproduct.php
@@ -10,7 +10,20 @@
     <body style="font-family: Poppins;">
 
     <!-- navbar -->
-    <?php include('includes/main/navbar.php'); ?>
+    <div class="contact-nav">
+    <div class="contact-nav-left"> <i class="fas fa-location-arrow" style= "color: #09427D; font-size: 13px;"></i> 052 P. Burgos St, Brgy. Lunsad, Binangonan, 1940 Rizal </div>
+    <div class="contact-nav-right"> <i class="fas fa-phone" style= "color: #09427D; font-size: 13px;"></i> Tel: 0915 512 5563  </div>
+    </div>
+    <nav>
+        <div class="logo">TorioRx</div>
+        <ul class="nav-links">
+            <li class="nav-links-li"><a href="user-dashboard.php" style="font-weight: 100">Home</a></li>
+            <li class="nav-links-li"><a href="user-about.php" style="font-weight: 100">About</a></li>
+            <li class="nav-links-li"><a href="products.php" style="font-weight: 100">Products</a></li>
+            <li class="nav-links-li"><a href="user-contact.php" style="font-weight: 100">Contact</a></li>
+            <li class="nav-links-li"><a href="registerlogin.php" style="font-weight: 100">Logout</a></li>
+        </ul>
+    </nav>
 
     <div class="container">
 		<div class="card">
