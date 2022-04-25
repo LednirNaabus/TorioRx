6bedff37bd69bcc3248cbbbd6c5790d5a42a8d33
Gabriela Andes <gabriela.andes05@gmail.com>
2022-04-25 18:49:16 +0800

    Customer Homepage Side

--- Customer/includes/main/navbar.php
+++ Customer/includes/main/navbar.php
@@ -1,3 +1,7 @@
+<div class="contact-nav">
+    <div class="contact-nav-left"> <i class="fas fa-location-arrow" style= "color: #09427D; font-size: 13px;"></i> 052 P. Burgos St, Brgy. Lunsad, Binangonan, 1940 Rizal </div>
+    <div class="contact-nav-right"> <i class="fas fa-phone" style= "color: #09427D; font-size: 13px;"></i> Tel: 0915 512 5563  </div>
+</div>
 <nav>
     <div class="logo">TorioRx</div>
     <input type="checkbox" id="screen-checker">
@@ -5,10 +9,10 @@
         <i class="fas fa-bars"></i>
     </label>
     <ul class="nav-links">
-        <li class="nav-links-li"><a href="#">Home</a></li>
-        <li class="nav-links-li"><a href="#">About</a></li>
-        <li class="nav-links-li"><a href="#">Products</a></li>
-        <li class="nav-links-li"><a href="#">Contact</a></li>
-        <li class="nav-links-li"><a href="#">Login</a></li>
+        <li class="nav-links-li"><a href="#" style="font-weight: 100">Home</a></li>
+        <li class="nav-links-li"><a href="#" style="font-weight: 100">About</a></li>
+        <li class="nav-links-li"><a href="#" style="font-weight: 100">Products</a></li>
+        <li class="nav-links-li"><a href="#" style="font-weight: 100">Contact</a></li>
+        <li class="nav-links-li"><a href="#" style="font-weight: 100">Login</a></li>
     </ul>
 </nav>
