53e5e84fb0585d9d981e2b9fcae159c4f9867dc5
Rindel Subaan <valsubaan@yahoo.com>
2022-04-29 00:55:51 +0800

    fix: add product image

--- Admin/product_add.php
+++ Admin/product_add.php
@@ -15,14 +15,14 @@
             Fatal error: Uncaught ValueError: Path cannot be empty
         */
         // $prod_img = mysqli_real_escape_string($link, file_get_contents($_FILES['product_img']['tmp_name']));
-        // $prod_img = addslashes(file_get_contents($_FILES['product_img']['tmp_name']));
+        $prod_img = addslashes(file_get_contents($_FILES['product_img']['tmp_name']));
         $prod_price = $_POST['product_price'];
         $prod_brief_info = $_POST['prod_bInfo'];
         $prod_desc = $_POST['product_desc'];
         $prod_content = $_POST['prod_contain'];
         $prod_presc = $_POST['prod_prescription'];
 
-        $sql_query = "INSERT INTO products (product_name, product_briefinfo, product_description, product_price, product_contains, product_prescription) VALUES ('$prod_name','$prod_brief_info', '$prod_desc', '$prod_price', '$prod_content', '$prod_presc')";
+        $sql_query = "INSERT INTO products (product_name, product_image, product_briefinfo, product_description, product_price, product_contains, product_prescription) VALUES ('$prod_name', '$prod_img','$prod_brief_info', '$prod_desc', '$prod_price', '$prod_content', '$prod_presc')";
         
         if(mysqli_query($link, $sql_query)) {
             ob_start();
