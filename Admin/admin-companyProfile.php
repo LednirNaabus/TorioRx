cce8f7bca2e27e6d31844f689a04ad764d89c589
rbellazar <87217997+rbellazar@users.noreply.github.com>
2022-05-02 22:16:40 +0800

    updated company profile

--- Admin/admin-companyProfile.php
+++ Admin/admin-companyProfile.php
@@ -41,9 +41,6 @@
 		<center>
 			<div class="container" style="margin-top:-10.6vh; font-family: 'Cutive Mono'; font-size: 35px; ">
 			  <div class="row">
-				<div class="col" style="background-color:rgba(25, 144, 206, 0.92); color: white; padding-top:2vh; padding-bottom:2vh;">
-				  <a href="#announcements" style="color: white; text-decoration: none;">ANNOUNCEMENTS</a>
-				</div>
 				<div class="col"  style="background-color:rgba(9, 64, 124, 0.94); color: white; padding-top:2vh; padding-bottom:2vh;">
 				  <a href="#aboutUs" style="color: white; text-decoration: none;">ABOUT US</a>
 				</div>
@@ -51,74 +48,6 @@
 				  <a href="#contactUs" style="color: white; text-decoration: none;">CONTACT US</a>
 				</div>
 			  </div>
-			</div>
-			<div id = "announcements"  class="container-fluid" style="padding-bottom: 10vh;">
-				<b><p style="font-family: Arial,Helvetica,sans-serif; font-size: 60px; color:#1990ce; z-index:1; padding-top: 10vh;">ANNOUNCEMENTS</p></b>
-				<div class="container-fluid" style="background-color:#1990ce; width:1vw; height:2.5vh;">
-					<p> &nbsp;</p>
-				</div>
-				<div id = "feedbacks" class="container-fluid">
-					<div class="container-fluid list-group" style="margin-top:10vh;">
-						 <a class="list-group-item list-group-item-action" aria-current="true">
-							<div class="d-flex w-100 justify-content-between">
-							  <h5 class="mb-1">Announcement Title</h5>
-							  <button type="button" style="float: right;" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
-									<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-										  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-										  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-									</svg>
-								</button>
-								<button type="button" style="float: right; margin-left:-74vw;" class="btn btn-primary">
-									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-									  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 
-										8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
-									</svg>
-								</button>
-							</div>
-							<p class="mb-1" >Some placeholder content in a paragraph.</p>
-							<small>Time/Date</small>
-						  </a>
-						  <a class="list-group-item list-group-item-action">
-							<div class="d-flex w-100 justify-content-between">
-							  <h5 class="mb-1">Announcement Title</h5>
-							  <button type="button" style="float: right;" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
-									<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-										  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-										  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-									</svg>
-								</button>
-								<button type="button" style="float: right; margin-left:-74vw;" class="btn btn-primary">
-									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-									  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 
-										8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
-									</svg>
-								</button>
-							</div>
-							<p class="mb-1">Some placeholder content in a paragraph.</p>
-							<small>Time/Date</small>
-						  </a>
-						  <a class="list-group-item list-group-item-action">
-							<div class="d-flex w-100 justify-content-between">
-							  <h5 class="mb-1">Announcement Title</h5>
-							  <button type="button" style="float: right;" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
-									<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-										  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-										  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-									</svg>
-								</button>
-								<button type="button" style="float: right; margin-left:-74vw;" class="btn btn-primary">
-									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-									  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 
-										8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
-									</svg>
-								</button>
-							</div>
-							<p class="mb-1">Some placeholder content in a paragraph.</p>
-							<small>Time/Date</small>
-					  </a>
-					</div>
-					<button type="button" class="btn btn-outline-primary" style="margin-top:7vh;">Add Announcement</button>
-				</div>
 			</div>
 			
 			<div id = "aboutUs" class="container-fluid background" style = "padding-bottom:15vh; padding-top:10vh;">
@@ -171,7 +100,7 @@
 					</button>
 				</div>
 			</div>
-				<div class="container-fluid" style="padding-bottom: 10vh;">
+				<div id="contactUs"class="container-fluid" style="padding-bottom: 10vh;">
 				<b><p style="font-family: Arial,Helvetica,sans-serif; font-size: 60px; color:#1990ce; z-index:1; padding-top: 10vh;">CONTACT US</p></b>
 				<button type="button" style="float: center; " class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal1">
 					<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
