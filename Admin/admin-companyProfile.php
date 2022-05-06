f8c36186b470a506aa6c244ea0df747414c864b2
rbellazar <87217997+rbellazar@users.noreply.github.com>
2022-05-07 03:20:33 +0800

    FINAL COMPANY PROFILE
    fixed fonts & some parts ng UI

--- Admin/admin-companyProfile.php
+++ Admin/admin-companyProfile.php
@@ -39,7 +39,7 @@
         <img src="img/pharmacy.jpeg" width="1600" height="660" style="border-bottom: 2px solid #09407c">
 		
 		<center>
-			<div class="container" style="margin-top:-10.6vh; font-family: 'Cutive Mono'; font-size: 35px; ">
+			<div class="container" style="margin-top:-10.6vh; font-size: 35px; ">
 			  <div class="row">
 				<div class="col"  style="background-color:rgba(9, 64, 124, 0.94); color: white; padding-top:2vh; padding-bottom:2vh;">
 				  <a href="#aboutUs" style="color: white; text-decoration: none;">ABOUT US</a>
@@ -51,11 +51,11 @@
 			</div>
 			
 			<div id = "aboutUs" class="container-fluid background" style = "padding-bottom:15vh; padding-top:10vh;">
-				<b><p style="font-family: Arial,Helvetica,sans-serif; font-size: 60px; color:white">ABOUT US</p></b>
+				<b><p style="font-size: 60px; color:white">ABOUT US</p></b>
 				<div class="container-fluid" style="background-color:white; width:1vw; height:2.5vh;">
 					<p> &nbsp;</p>
 				</div>
-				<div class="container" style="margin-top:5vh; font-family: 'Poppins'; font-size: 19px; width: 65vw; ">
+				<div class="container" style="margin-top:5vh; font-size: 19px; width: 65vw; ">
 					  <div class="row">
 						<div class="col" style="color: white; padding-right: 3vw;">
 								<br>
@@ -101,104 +101,39 @@
 				</div>
 			</div>
 				<div id="contactUs"class="container-fluid" style="padding-bottom: 10vh;">
-				<b><p style="font-family: Arial,Helvetica,sans-serif; font-size: 60px; color:#1990ce; z-index:1; padding-top: 10vh;">CONTACT US</p></b>
-				<button type="button" style="float: center; " class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal1">
-					<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-						  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-						  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-					</svg>
-				</button>
+				<b><p style="font-size: 60px; color:#1990ce; z-index:1; padding-top: 10vh;">CONTACT US</p></b>
+				<div class="container-fluid" style="background-color:#1990ce; width:1vw; height:2.5vh;">
+					<p> &nbsp;</p>
+				</div>
 				<div class="container" style="margin-top:10vh; ">
 					  <div class="row">
 						<div class="col" style="border-right: 1px solid grey; padding-right: 3vw;">
 								<br><br>
-							<b><p style="color: #1990ce; font-size: 25px; font-family: 'Raleway';">Our Address</p></b>
+							<b><p style="color: #1990ce; font-size: 27px;">Our Address</p></b>
 							
 								<br>
 								
-							<p style="color: #6f6767; font-family: 'Poppins'; font-size: 17px;">052 P. Burgos St, Brgy. Lunsad, Binangonan, 1940 Rizal</p>
+							<p style="color: #6f6767; font-size: 17px;">052 P. Burgos St, Brgy. Lunsad, Binangonan, 1940 Rizal</p>
 								
-							<p style="color: #6f6767; font-family: 'Poppins'; font-size: 17px;">TorioRx@gmail.com</p>
+							<p style="color: #6f6767; font-size: 17px;">TorioRx@gmail.com</p>
 							
-							<p style="color: #6f6767; font-family: 'Poppins'; font-size: 17px;">Contact: 0915 512 5563</p>
+							<p style="color: #6f6767; font-size: 17px;">Contact: 0915 512 5563</p>
 							
 							<br><br>
 						</div>
-						<div class="col"  style="color: grey; font-family: 'Raleway'; padding-left:5vh;">
+						<div class="col"  style="color: grey; padding-left:5vh;">
 								<br><br>
-							<b><p style="color: #1990ce; font-size: 25px; font-family: 'Raleway';">Opening Hours</p></b>
+							<b><p style="color: #1990ce; font-size: 27px;">Opening Hours</p></b>
 								
 								<br><br>
-							<p style="color: #6f6767; font-family: 'Poppins'; font-size: 17px;">Monday - Sunday: 8AM - 9PM</p>
+							<p style="color: #6f6767; font-size: 17px;">Monday - Sunday: 8AM - 9PM</p>
 							
-							<p style="color: #6f6767; font-family: 'Poppins'; font-size: 17px;">Labour Day: Hours might differ</p>
+							<p style="color: #6f6767; font-size: 17px;">Labour Day: Hours might differ</p>
 							<br><br>
 						</div>
 							
 						<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15445.32872877058!2d121.1166629!3d14.580138!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1ffc06b52e2658b!2sTorio%20Rx%20Pharmacy!5e0!3m2!1sen!2sph!4v1650863560833!5m2!1sen!2sph" width="600" height="450" style="border:0; margin-top: 10vh;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 					  </div>
-					</div>
-				</div>
-				
-				<div class="modal fade" id="myModal" role="dialog">
-					<div class="modal-dialog modal-lg">
-					<div class="modal-content">
-						<div class="modal-header">
-						<button type="button" class="close" data-bs-dismiss="modal">&times;</button>
-						<h4 class="modal-title">Edit Announcement</h4>
-						</div>
-						<div class="modal-body">
-						<form>
-						<div class="form-group">
-							<label for="exampleFormControlInput1" >Announcement Title</label>
-							<input style="margin-left:30px; width:90%; margin-top:2vh;" type="text" class="form-control" id="exampleFormControlInput1">
-						</div>
-						<div class="form-group">
-							<label for="exampleFormControlInput1" style="margin-top:3vh;">Announcement Content</label>
-							<textarea class="form-control" id="floatingTextarea2" style="width:90%; margin-left:2vw;"></textarea>
-						</div>
-						<div class="form-group">
-							<label for="exampleFormControlInput1" style="margin-top:3vh;">Announcement Time/Date</label>
-							<input style="margin-left:30px; width:90%; margin-top:2vh;" type="text" class="form-control" id="exampleFormControlInput1">
-						</div>
-						</form>
-						</div>
-						<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-bs-dismiss="modal">Discard</button>
-						<button type="button" class="btn btn-default">Save</button>
-						</div>
-					</div>
-					</div>
-				</div>
-				
-				<div class="modal fade" id="myModal1" role="dialog">
-					<div class="modal-dialog modal-lg">
-					<div class="modal-content">
-						<div class="modal-header">
-						<button type="button" class="close" data-bs-dismiss="modal">&times;</button>
-						<h4 class="modal-title">Edit Pharmacy Details</h4>
-						</div>
-						<div class="modal-body">
-						<form>
-						<div class="form-group">
-							<label for="exampleFormControlInput1" >Pharmacy Address</label>
-							<input style="margin-left:30px; width:90%; margin-top:2vh;" type="text" class="form-control" id="exampleFormControlInput1">
-						</div>
-						<div class="form-group">
-							<label for="exampleFormControlInput1" style="margin-top:3vh;">Pharmacy Email</label>
-							<input style="margin-left:30px; width:90%; margin-top:2vh;" type="text" class="form-control" id="exampleFormControlInput1">
-						</div>
-						<div class="form-group">
-							<label for="exampleFormControlInput1" style="margin-top:3vh;">Pharmacy Contact Number</label>
-							<input style="margin-left:30px; width:90%; margin-top:2vh;" type="text" class="form-control" id="exampleFormControlInput1">
-						</div>
-						</form>
-						</div>
-						<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-bs-dismiss="modal">Discard</button>
-						<button type="button" class="btn btn-default">Save</button>
-						</div>
-					</div>
 					</div>
 				</div>
 		</center>
