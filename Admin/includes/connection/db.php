3d3caf7deff3e6e3dd984ba40f780de537187a29
Rindel Subaan <valsubaan@yahoo.com>
2022-05-06 23:14:23 +0800

    edit: changed database name

--- Admin/includes/connection/db.php
+++ Admin/includes/connection/db.php
@@ -3,7 +3,7 @@
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'toriorx-db');
+    define('DB_NAME', 'epiz_31667370_toriorxdb');
 
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 ?>
